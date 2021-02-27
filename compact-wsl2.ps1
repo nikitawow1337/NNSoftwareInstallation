cb5783bd7b7ec1bf9e17b5e1737d610866cac0b5
Nikita Bazhenov <nekit1337@yandex.ru>
2021-02-28 00:49:04 +0300

    Add script for cleaning memory in WSL

---
+++ compact-wsl2.ps1
@@ -0,0 +1,38 @@
+$ErrorActionPreference = "Stop"
+
+# File is normally under something like C:\Users\onoma\AppData\Local\Packages\CanonicalGroupLimited...
+$files = @()
+cd $env:LOCALAPPDATA\Packages
+get-childitem -recurse -filter "ext4.vhdx" -ErrorAction SilentlyContinue | foreach-object {
+  $files += ${PSItem}
+}
+
+
+
+if ( $files.count -eq 0 ) {
+  throw "We could not find a file called ext4.vhdx in $env:LOCALAPPDATA\Packages or $env:LOCALAPPDATA\Docker"
+}
+
+write-output " - Found $($files.count) VHDX file(s)"
+write-output " - Shutting down WSL2"
+
+# See https://github.com/microsoft/WSL/issues/4699#issuecomment-722547552
+wsl -e sudo fstrim /
+wsl --shutdown
+
+foreach ($file in $files) {
+	$disk = $file.FullName
+	
+	write-output " - Compacting disk (starting diskpart)"
+
+@"
+select vdisk file=$disk
+attach vdisk readonly
+compact vdisk
+detach vdisk
+exit
+"@ | diskpart
+
+	write-output ""
+	write-output "Success. Compacted $disk."
+}
